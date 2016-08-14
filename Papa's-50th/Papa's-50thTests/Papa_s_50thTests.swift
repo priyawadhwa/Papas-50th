46984e939608873051f75898d593fddb704dc66d
priyawadhwa <priyawadhwa@Priyas-Air.frontier.com>
2016-08-14 16:01:33 -0400

    Fresh start

---
+++ Papa's-50th/Papa's-50thTests/Papa_s_50thTests.swift
@@ -0,0 +1,36 @@
+//
+//  Papa_s_50thTests.swift
+//  Papa's-50thTests
+//
+//  Created by Priya Wadhwa on 8/14/16.
+//  Copyright © 2016 Priya Wadhwa. All rights reserved.
+//
+
+import XCTest
+@testable import Papa_s_50th
+
+class Papa_s_50thTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
