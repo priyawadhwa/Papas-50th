61e120a6ae9f9e5061b7a00a3735551685db07cc
priyawadhwa <priyawadhwa@Priyas-Air.frontier.com>
2016-08-15 16:22:41 -0400

    Added video capability to videoviewcontroller

--- Papa's-50th/Papa's-50th/VideoViewController.swift
+++ Papa's-50th/Papa's-50th/VideoViewController.swift
@@ -7,11 +7,30 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class VideoViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func pushButton1(sender: UIButton) {
+        self.playVideo()
+    }
 
+    
+    func playVideo() {
+        let path = NSBundle.mainBundle().pathForResource("Bua", ofType:"mov")
+        print(path)
+        let player = AVPlayer(URL: NSURL(fileURLWithPath: path!))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        self.presentViewController(playerController, animated: true) {
+            player.play()
+        }
+    }
 }
