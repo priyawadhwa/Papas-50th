4c86573552b2c5c1695f349107eefe6c9fbe8e0b
priyawadhwa <priyawadhwa@Priyas-Air.frontier.com>
2016-08-15 15:11:42 -0400

    removed code

--- Papa's-50th/Papa's-50th/TabViewController.swift
+++ Papa's-50th/Papa's-50th/TabViewController.swift
@@ -12,34 +12,6 @@ class TabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let firstViewController:UIViewController = PhotoViewController()
-        
-        let customTabBarItem:UITabBarItem = UITabBarItem(title: nil, image: UIImage(named: "Photos")?.imageWithRenderingMode(.AlwaysOriginal), tag: 1)
-        
-        firstViewController.tabBarItem = customTabBarItem
-        
-        let secondViewController:UIViewController = VideoViewController()
-        
-        let secondCustomTabBarItem:UITabBarItem = UITabBarItem(title: nil, image: UIImage(named: "Video")?.imageWithRenderingMode(.AlwaysOriginal), tag: 2)
-        
-        secondViewController.tabBarItem = secondCustomTabBarItem
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }
